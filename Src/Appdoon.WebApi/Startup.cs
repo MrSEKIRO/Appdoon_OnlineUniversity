ce95c7e9627aa52dfebf0ef431734f8468a0186f
MrSEKIRO <ab.arshia@gmail.com>
2022-04-05 00:03:28 +0430

    delete WeatherForcastController and make a change of startup.cs

--- Src/Appdoon.WebApi/Startup.cs
+++ Src/Appdoon.WebApi/Startup.cs
@@ -26,6 +26,7 @@ namespace OU_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Facad Pattern
 
             //Enable CORS
             services.AddCors(c =>
