99137d848b66e362a84e3c0185a5824ec4d85f63
MrSEKIRO <ab.arshia@gmail.com>
2022-04-04 20:59:25 +0430

    make projects

---
+++ Src/Appdoon.WebApi/Models/Services/Register/IRegisterService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using OU_API.Models.Contexts;
+using OU_API.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OU_API.Models.Services.Register
+{
+    public interface IRegisterService
+    {
+        JsonResult Execute(User user);
+    }
+
+    public class RegisterService : IRegisterService
+    {
+        private readonly DatabaseContext _databaseContext;
+
+        public RegisterService(IConfiguration configuration)
+        {
+            this._databaseContext = new DatabaseContext(configuration);
+        }
+
+        public JsonResult Execute(User user)
+        {
+            _databaseContext.Users.Add(user);
+            _databaseContext.SaveChanges();
+            return new JsonResult("Registered Successfully");
+        }
+
+    }
+
+
+    //public class RegisterDto
+    //{
+    //
+    //}
+}
