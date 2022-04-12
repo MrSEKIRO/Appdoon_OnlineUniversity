c175930b0dc0aadfdf8941189b18dd7a979f0aa4
MrSEKIRO <ab.arshia@gmail.com>
2022-04-12 12:50:33 +0430

    create data models and two apis

---
+++ Src/Appdoon.Common/UserRoles/UserRoles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appdoon.Common.UserRoles
+{
+	public enum UserRole
+	{
+		Admin = 1,
+		Teacher,
+		User,
+	}
+}
