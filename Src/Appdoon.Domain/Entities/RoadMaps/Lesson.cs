cf72a690709a5d2c363a2938f0feced8bd47dc3f
MrSEKIRO <ab.arshia@gmail.com>
2022-05-09 14:37:49 +0430

    add Lesson and Linker entities

---
+++ Src/Appdoon.Domain/Entities/RoadMaps/Lesson.cs
@@ -0,0 +1,16 @@
+﻿using Appdoon.Domain.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appdoon.Domain.Entities.RoadMaps
+{
+	public class Lesson : BaseEntity
+	{
+		public string Title { get; set; } = string.Empty;
+		public string TopBannerSrc { get; set; } = string.Empty;
+		public string Text { get; set; } = string.Empty;
+	}
+}
---
+++ Src/Appdoon.Presistence/Migrations/20220509095543_Add_Linker_and_Lesson.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Appdoon.Presistence.Migrations
+{
+    public partial class Add_Linker_and_Lesson : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lessons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TopBannerSrc = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InsertTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsRemoved = table.Column<bool>(type: "bit", nullable: false),
+                    RemoveTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdateTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lessons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Linkers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Link = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InsertTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsRemoved = table.Column<bool>(type: "bit", nullable: false),
+                    RemoveTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdateTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Linkers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChildStepLinker",
+                columns: table => new
+                {
+                    ChildStepsId = table.Column<int>(type: "int", nullable: false),
+                    LinkersId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChildStepLinker", x => new { x.ChildStepsId, x.LinkersId });
+                    table.ForeignKey(
+                        name: "FK_ChildStepLinker_ChildSteps_ChildStepsId",
+                        column: x => x.ChildStepsId,
+                        principalTable: "ChildSteps",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ChildStepLinker_Linkers_LinkersId",
+                        column: x => x.LinkersId,
+                        principalTable: "Linkers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "InsertTime",
+                value: new DateTime(2022, 5, 9, 14, 25, 42, 174, DateTimeKind.Local).AddTicks(678));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "InsertTime",
+                value: new DateTime(2022, 5, 9, 14, 25, 42, 182, DateTimeKind.Local).AddTicks(3059));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "InsertTime",
+                value: new DateTime(2022, 5, 9, 14, 25, 42, 182, DateTimeKind.Local).AddTicks(3424));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChildStepLinker_LinkersId",
+                table: "ChildStepLinker",
+                column: "LinkersId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChildStepLinker");
+
+            migrationBuilder.DropTable(
+                name: "Lessons");
+
+            migrationBuilder.DropTable(
+                name: "Linkers");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "InsertTime",
+                value: new DateTime(2022, 5, 1, 16, 41, 55, 281, DateTimeKind.Local).AddTicks(1783));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "InsertTime",
+                value: new DateTime(2022, 5, 1, 16, 41, 55, 287, DateTimeKind.Local).AddTicks(2176));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "InsertTime",
+                value: new DateTime(2022, 5, 1, 16, 41, 55, 287, DateTimeKind.Local).AddTicks(2556));
+        }
+    }
+}
